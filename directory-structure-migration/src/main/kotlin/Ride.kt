acf15329314837d87a8a8857baaac7d72a2e10c7
selphiron <a.karakaya@tu-berlin.de>
2021-07-07 12:55:06 +0200

    Takes the older TS of either creationTime or lastModifiedTime to determine file creation time

--- directory-structure-migration/src/main/kotlin/Ride.kt
+++ directory-structure-migration/src/main/kotlin/Ride.kt
@@ -1,9 +1,7 @@
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.attribute.FileTime
-import java.time.LocalDateTime
-import java.time.Month
-import java.time.ZoneId
+import java.time.*
 
 class Ride(val originalFile: File) {
 
@@ -14,13 +12,23 @@ class Ride(val originalFile: File) {
         }
     }
 
-    private val creationTime = Files.getAttribute(originalFile.toPath(), "creationTime") as FileTime
-    val date = creationTime.toInstant().atZone(ZoneId.of("Europe/Berlin")).apply {
-        check(this.year <= LocalDateTime.now().year) { "Year must be smaller than/equal to than the current year"}
-        check(this.year >= 2018) { "Year must be larger than/equal to 2018"}
 
-        check(this.month.value <= 12) { "Month must be smaller smaller than/equal to 12"}
-        check(this.month.value >= 1) { "Month must be larger than/equal to 1"}
+    private val date = getOlderDate()
+
+    private fun getOlderDate(): ZonedDateTime {
+        val creationTime = Files.getAttribute(originalFile.toPath(), "creationTime") as FileTime
+        val lastModifiedTime = Files.getAttribute(originalFile.toPath(), "lastModifiedTime") as FileTime
+        val olderTime: FileTime = if (creationTime.toMillis() < lastModifiedTime.toMillis()) {
+            creationTime;
+        } else {
+            lastModifiedTime;
+        }
+        return olderTime.toInstant().atZone(ZoneId.of("Europe/Berlin")).apply {
+            check(this.year <= LocalDateTime.now().year) { "Year must be smaller than/equal to than the current year"}
+            check(this.year >= 2018) { "Year must be larger than/equal to 2018"}
+            check(this.month.value <= 12) { "Month must be smaller smaller than/equal to 12"}
+            check(this.month.value >= 1) { "Month must be larger than/equal to 1"}
+        }
     }
 
     fun getTargetFilePath(): String {
@@ -28,7 +36,7 @@ class Ride(val originalFile: File) {
         return "${split[0]}${File.separator}Rides${File.separator}${date.year}${File.separator}${date.month.twoDigits()}${File.separator}${split[1]}"
     }
 
-    fun Month.twoDigits(): String {
+    private fun Month.twoDigits(): String {
         return String.format("%02d", this.value)
     }
 
